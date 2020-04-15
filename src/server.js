60edd960a0aee67a94df943e07883dfa2d22f503
Shoaib Sharif <sharifshoaib@yahoo.com>
2020-04-14 20:56:20 -0700

    fixed, eibar kaj korbe

--- src/server.js
+++ src/server.js
@@ -1,73 +1,72 @@
 import {
-	ApolloServer,
-	ApolloError,
-	makeExecutableSchema
-} from 'apollo-server-express'
-import './connection'
-import express from 'express'
-import { rule, shield } from 'graphql-shield'
-import { applyMiddleware } from 'graphql-middleware'
+  ApolloServer,
+  ApolloError,
+  makeExecutableSchema,
+} from "apollo-server-express";
+import "./connection";
+import express from "express";
+import { rule, shield } from "graphql-shield";
+import { applyMiddleware } from "graphql-middleware";
+import { verify } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config(); // enable env
 
-import dotenv from 'dotenv'
-dotenv.config() // enable env
+import Mutation from "./mutations/Mutations";
+import Query from "./query/Query";
+import Thread from "./query/Thread";
+import { importSchema } from "graphql-import";
+import formatMongooseValidationError from "./formatMongooseValidationError";
+import AppError from "./AppError";
 
-import Mutation from './mutations/Mutations'
-import Query from './query/Query'
-import Thread from './query/Thread'
-import { importSchema } from 'graphql-import'
-import formatMongooseValidationError from './formatMongooseValidationError'
-import AppError from './AppError'
-
-const app = express()
+const app = express();
 
 const schema = makeExecutableSchema({
-	typeDefs: importSchema(__dirname + '/gql/typeDefs.graphql'),
-	resolvers: { Mutation, Query, Thread }
-})
+  typeDefs: importSchema(__dirname + "/gql/typeDefs.graphql"),
+  resolvers: { Mutation, Query, Thread },
+});
+const isAuthenticated = rule()((parent, args, { req }, info) => {
+  if (!req.headers.authorization) return false;
 
+  let token = req.headers.authorization.replace("Bearer ", "");
+  if (!token) return false;
+  let decoded = verify(token, process.env.APP_SECRET);
+  console.log(token);
+  console.log(decoded);
+  if (!decoded) {
+    return false;
+  }
+
+  return true;
+});
 const permissions = shield({
-	Mutation: {
-		createThread: isAuthenticated
-	}
-})
+  Mutation: {
+    createThread: isAuthenticated,
+  },
+});
 
-const middlewares = applyMiddleware(schema, permissions)
+const middlewares = applyMiddleware(schema, permissions);
 
 let server = new ApolloServer({
-	// typeDefs: importSchema(__dirname + '/gql/typeDefs.graphql'),
-	// resolvers: { Mutation, Query, Thread },
-	schema: middlewares,
-	context: (expressParams) => ({ ...expressParams }),
-	engine: {
-		rewriteError(err) {
-			return new AppError(err.message)
-		}
-	}
-	// formatError(err) {
-	// 	// if (err.extensions.exception.name === 'ValidationError') {
-	// 	// 	return 'Hello'
-	// 	// }
-	// 	// err.errors = formatMongooseValidationError(err.extensions.exception.errors)
-	// 	// return new AppError('Hello', { email: 'invalid email' }, 'VALDAddhskdjs')
-	// }
-})
+  // typeDefs: importSchema(__dirname + '/gql/typeDefs.graphql'),
+  // resolvers: { Mutation, Query, Thread },
+  schema: middlewares,
+  context: ({ req }) => ({ req }),
+  engine: {
+    rewriteError(err) {
+      return new AppError(err.message);
+    },
+  },
+  // formatError(err) {
+  // 	// if (err.extensions.exception.name === 'ValidationError') {
+  // 	// 	return 'Hello'
+  // 	// }
+  // 	// err.errors = formatMongooseValidationError(err.extensions.exception.errors)
+  // 	// return new AppError('Hello', { email: 'invalid email' }, 'VALDAddhskdjs')
+  // }
+});
 
-const isAuthenticated = rule()((parent, args, ctx, info) => {
-	if (!request.headers.authorization) return false
-
-	let token = request.headers.authorization.replace('Bearer ', '')
-	if (!token) return false
-	let decoded = verify(token, process.env.APP_SECRET)
-
-	if (!decoded) {
-		return false
-	}
-
-	return true
-})
-
-server.applyMiddleware({ app })
+server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () => {
-	console.log(`http://localhost:4000${server.graphqlPath}`)
-})
+  console.log(`http://localhost:4000${server.graphqlPath}`);
+});
